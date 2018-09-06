f8171861d023aba9059cf16f2ae3317b3c2a77a5
Shouraa <shorajafar@gmail.com>
2018-09-06 11:41:33 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './Components/Header';
-import CoinCointainer from './Components/CoinContainer';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import CoinCointainer from "./Components/CoinContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-      <CoinCointainer />
+        <Header />
+        <CoinCointainer />
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,45 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return(
-            <div className="header">
-            <h1>CryptoCurrency App</h1>
-          
-
-
-            </div>
-
-        );
-    }
- 
-};
+  render() {
+    return (
+      <div className="header">
+        <h1>CryptoCurrency App</h1>
+      </div>
+    );
+  }
+}
 
 export default Header;
-
-
-
-
-
-
-// constructor(props) {
-//     super(props);
-//     this.state = {
-//         itemCount: 0
-//     };
-//     this.addItem = this.addItem.bind(this);
-    
-// }
-// addItem() {
-//     this.setState({
-//         itemCount: this.state.itemCount + 1
-//     });
-// }
-// render() {
-//     return (
-//         <div>
-//             <button onClick={this.addItem}>Click!</button>
-//             <h1>Current Item count is : {this.state.itemCount}</h1>
-//         </div>
-//     );
-// }
